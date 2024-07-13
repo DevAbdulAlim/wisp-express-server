b9867fc26e6d881e17da7bed49f8b6a21f2eb0c8
Abdul Alim <aa.abdulalim13@gmail.com>
2024-07-14 04:08:43 +0600

    added room

--- server.ts
+++ server.ts
@@ -1,24 +1,40 @@
-import express from 'express';
-import http from 'http';
-import {Server, Socket} from 'socket.io';
+import express from "express";
+import http from "http";
+import { Server, Socket } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-io.on('connection', (socket: Socket) => {
-    console.log('A user connected');
+io.on("connection", (socket: Socket) => {
+  console.log("A user connected");
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    })
+  // Joining a room
+  socket.on("join room", (room: string) => {
+    socket.join(room);
+    console.log(`User joined room: ${room}`);
+  });
 
-    socket.on('chat message', (message: string) => {
-        io.emit('chat message', message);
-    })
-})
+  // Leaving a room
+  socket.on("leave room", (room: string) => {
+    socket.leave(room);
+    console.log(`User left room: ${room}`);
+  });
+
+  // Sending a message to a specific room
+  socket.on(
+    "chat message",
+    ({ room, message }: { room: string; message: string }) => {
+      io.to(room).emit("chat message", message);
+    }
+  );
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
-    console.log(`Connected to ${PORT}`)
+  console.log(`Connected to ${PORT}`);
 });
